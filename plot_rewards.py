31c5845757e900ca01f82021c180d49f73ec4a0f
Annie Xie <axie0906@gmail.com>
2019-10-05 23:49:54 -0700

    plot

--- plot_rewards.py
+++ plot_rewards.py
@@ -4,10 +4,10 @@ plt.switch_backend('agg')
 import os
 
 
-def plot(result_dir='ray_results'):
+def plot(result_dir='ray_results/gym'):
 	paths = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(result_dir)) for f in fn if 'csv' in f]
 	rewards = []
-	for path in paths[::-1]:
+	for path in paths:
 		df = pd.read_csv(path, sep=',', engine='python', header=0)
 		plt.plot(df['training/episode-reward-mean'], label=path.split('/')[4])
 	plt.legend(loc='lower right')
